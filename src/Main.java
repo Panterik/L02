7cf3d444aa82b7507ca763e492bd78abb54d5910
Aidar <adic.2020@mail.ru>
2023-02-21 22:31:01 +0300

    L02 - added Hello program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
     }
 }
