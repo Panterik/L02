ce95e26dc3745e9cf1b15f5886a4c321f479a7bf
Aidar <adic.2020@mail.ru>
2023-02-21 22:40:27 +0300

    L02 - added first Matematic formula

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
         System.out.println("Введите имя: ");
         String name = sc.nextLine();
         System.out.println("Hello " + name);
+        System.out.println("Введите два вещественных числа:");
+        double first = sc.nextDouble();
+        double second = sc.nextDouble();
+        double sum = 0.0;
+        sum = 3 * Math.pow(Math.cos(first - (Math.PI / 6)), 2) / (0.5 + Math.sin(Math.pow(second, 2)));
+        System.out.println("Ответ: " + sum);
     }
 }
